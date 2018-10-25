14ff2b6881dbf15d90aa000b55c585f84e55ba46
megha <megha.libra82@gmail.com>
2018-10-25 21:41:44 +0530

    First commit

---
+++ app/src/main/java/com/mobiwiz/smartbuy/activities/Register.java
@@ -0,0 +1,24 @@
+package com.mobiwiz.smartbuy.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.mobiwiz.smartbuy.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
